7ae9886a56891d321e3dcce0b4ef1af634dbf933
Theon87 <theonfrancis17@gmail.com>
2024-11-22 22:32:25 -0500

    updated the contanct section with contact info

--- src/components/pages/Contact.jsx
+++ src/components/pages/Contact.jsx
@@ -2,7 +2,18 @@ import React from 'react'
 
 const Contact = () => {
   return (
-    <div>Contact</div>
+    <div className='contact'>
+      <h1>Contact</h1>
+      <p>
+        Github: <a href='https://github.com/Theon87'>Theon87</a>
+      </p>
+      <p>
+        Email: theonfrancis17@gmail.com
+      </p>
+      <p>
+        Phone: 410-805-4600
+      </p>
+    </div>
   )
 }
 
