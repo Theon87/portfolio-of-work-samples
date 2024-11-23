79259be9cc397b96f419cb6ca17844b9d52c17cb
Theon87 <theonfrancis17@gmail.com>
2024-11-22 22:33:56 -0500

    updated the jsx to include a p tag with placeholder text

--- src/components/pages/Resume.jsx
+++ src/components/pages/Resume.jsx
@@ -2,7 +2,14 @@ import React from 'react'
 
 const Resume = () => {
   return (
-    <div>Resume</div>
+    <div className='resume'>
+      <h1>Resume</h1>
+      <p>
+        ipsom Lorem, ipsum dolor sit amet consectetur adipisicing elit. 
+        Delectus non alias praesentium tempora, tenetur nostrum vel impedit tempore, 
+        eligendi cumque aliquam voluptatem autem sapiente natus esse nobis dolorum veritatis atque.
+      </p>
+    </div>
   )
 }
 
