976de4ac21bc069affe0d66c5b2dd4b05d556292
Theon87 <theonfrancis17@gmail.com>
2024-11-22 22:33:11 -0500

    updated the jsx to include a p tag with placeholder text

--- src/components/pages/Portfolio.jsx
+++ src/components/pages/Portfolio.jsx
@@ -2,7 +2,14 @@ import React from 'react'
 
 const Portfolio = () => {
   return (
-    <div>Portfolio</div>
+    <div className='portfolio'>
+      <h1>Portfolio</h1>
+      <p>
+        ipsom Lorem, ipsum dolor sit amet consectetur adipisicing elit. 
+        Delectus non alias praesentium tempora, tenetur nostrum vel impedit tempore, 
+        eligendi cumque aliquam voluptatem autem sapiente natus esse nobis dolorum veritatis atque.
+      </p>
+    </div>
   )
 }
 
