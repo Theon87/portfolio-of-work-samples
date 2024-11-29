1a721d1e3726cc39cc9950e80137ff62384aa902
Theon87 <theonfrancis17@gmail.com>
2024-11-29 15:20:35 -0500

    add email link to contact page

--- src/components/pages/Contact.jsx
+++ src/components/pages/Contact.jsx
@@ -2,18 +2,15 @@ import React from 'react'
 
 const Contact = () => {
   return (
-    <div className='contact'>
+    <>
       <h1>Contact</h1>
-      <p>
-        Github: <a href='https://github.com/Theon87'>Theon87</a>
-      </p>
-      <p>
-        Email: theonfrancis17@gmail.com
-      </p>
-      <p>
-        Phone: 410-805-4600
-      </p>
-    </div>
+      <div className='contact'>
+        <p>
+          Email:
+          <a href='mailto: theonfrancis17@gmail.com'> theonfrancis17@gmail.com</a>
+        </p>
+      </div>
+    </>
   )
 }
 
