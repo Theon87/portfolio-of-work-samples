909f5f5b27c2d40d44212a9b5c49b7f11ad86ce9
Theon87 <theonfrancis17@gmail.com>
2024-11-29 15:15:02 -0500

    removed unnecessary comments

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -23,7 +23,7 @@ function Navigation ({ currentPage, handlePageChange }) {
       </li>
       <li className="nav-item">
         <a
-          href="#Project"
+          href="#Contact"
           onClick={() => handlePageChange('Contact')}
           className={currentPage === 'Contact' ? 'nav-link active' : 'nav-link'}
         >
@@ -33,8 +33,6 @@ function Navigation ({ currentPage, handlePageChange }) {
       <li className="nav-item">
         <a
           href="#Resume"
-          //  TODO: Add a comment explaining what this logic is doing
-          // This is a ternary operator that checks if the currentPage is equal to 'Contact' and if it is, it adds the 'active' class to the nav-link
           onClick={() => handlePageChange('Resume')}
           className={currentPage === 'Resume' ? 'nav-link active' : 'nav-link'}
         >
@@ -46,35 +44,3 @@ function Navigation ({ currentPage, handlePageChange }) {
 }
 
 export default Navigation
-
-// GIVEN a single-page application portfolio for a web developer
-
-// WHEN I load the portfolio
-// THEN I am presented with a page containing a header, a section for content, and a footer
-
-// WHEN I click on a navigation title
-// THEN I am presented with the corresponding section below the navigation without the page reloading and that title is highlighted
-
-// WHEN I load the portfolio the first time
-// THEN the About Me title and section are selected by default
-
-// WHEN I am presented with the About Me section
-// THEN I see a recent photo or avatar of the developer and a short bio about them
-
-// WHEN I am presented with the Portfolio section
-// THEN I see titled images of six of the developer’s applications with links to both the deployed applications and the corresponding GitHub repository
-
-// WHEN I am presented with the Contact section
-// THEN I see a contact form with fields for a name, an email address, and a message
-
-// WHEN I move my cursor out of one of the form fields without entering text
-// THEN I receive a notification that this field is required
-
-// WHEN I enter text into the email address field
-// THEN I receive a notification if I have entered an invalid email address
-
-// WHEN I am presented with the Resume section
-// THEN I see a link to a downloadable resume and a list of the developer’s proficiencies
-
-// WHEN I view the footer
-// THEN I am presented with text or icon links to the developer’s GitHub and LinkedIn profiles, and their profile on a third platform (Stack Overflow, Twitter) 
