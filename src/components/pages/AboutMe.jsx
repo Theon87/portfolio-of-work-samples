2c2c0423fcc2d5c66c6951df66b5fbea3aa6ec52
Theon87 <theonfrancis17@gmail.com>
2024-11-29 15:18:54 -0500

    add profile photo and text about me

--- src/components/pages/AboutMe.jsx
+++ src/components/pages/AboutMe.jsx
@@ -2,13 +2,20 @@ import React from 'react'
 
 const AboutMe = () => {
   return (
-    <div className='about-me'>
-        <h1>AboutMe</h1>
-        <p>
-          I am a motivated <b>Full Stack Web Developer</b> with a strong foundation in education and proven expertise in analytical thinking and team collaboration. Adept at solving complex problems and delivering impactful solutions through effective communication, both written and verbal.
-        </p>
-    </div>
+    <>
+      <h1>About Me</h1>
+      <div className='about-me'>
+          <img src='../src/assets/profile_photo.jpg' alt='photo of Theon Francis' className='profilePhoto'/>
+          <p>
+            I was born and raised in Trinidad and Tobago, studied biology at Lincoln University as an undergrad, returned to Trinidad to work in education, came back to Maryland to work in education for seven years, and finally moved into full-stack web development. I love watching and playing football (soccer) and have coached the sport for seven years. In my spare time, I like to play chess for fun.
+          </p>
+          <p>
+            I made a career change to become a full-stack web developer. As a Columbia Engineering Coding Bootcamp graduate, I have acquired the skills to build robust applications using a wide range of modern technologies. I love this! I really enjoy the challenge of solving complex problems and the immediate satisfaction of fixing a bug. I also love the collaboration needed to meet the needs of the end user. After working on over thirty projects, my eyes are now primed for the web development world.
+          </p>
+      </div>
+    </>
   )
 }
 
 export default AboutMe
+
