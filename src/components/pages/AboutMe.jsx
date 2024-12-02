c40e5ee173d305f5d26d6ae05329a783d4bc5c76
Theon87 <theonfrancis17@gmail.com>
2024-12-02 16:07:48 -0500

    update about me

--- src/components/pages/AboutMe.jsx
+++ src/components/pages/AboutMe.jsx
@@ -7,10 +7,10 @@ const AboutMe = () => {
       <div className='about-me'>
           {/* <img src='../src/assets/images/profilephoto.jpg' alt='photo of Theon Francis' className='profilePhoto'/> */}
           <p>
-            I was born and raised in Trinidad and Tobago, studied biology at Lincoln University as an undergrad, returned to Trinidad to work in education, came back to Maryland to work in education for seven years, and finally moved into full-stack web development. I love watching and playing football (soccer) and have coached the sport for seven years. In my spare time, I like to play chess for fun.
+            I was born and raised in Trinidad and Tobago. I love to travel, so I thought, why not move to the United States to study biology at Lincoln University? After completing my undergraduate degree, I returned to Trinidad to work in education, and then I came back to Maryland to work in education for seven more years. I love watching and playing football (soccer) and have coached the sport for seven years. In my spare time, I also like to play chess for fun.
           </p>
           <p>
-            I made a career change to become a full-stack web developer. As a Columbia Engineering Coding Bootcamp graduate, I have acquired the skills to build robust applications using a wide range of modern technologies. I love this! I really enjoy the challenge of solving complex problems and the immediate satisfaction of fixing a bug. I also love the collaboration needed to meet the needs of the end user. After working on over thirty projects, my eyes are now primed for the web development world.
+            Now, I am taking the big step to change my career to a full-stack web developer. As a Columbia Engineering Coding Bootcamp graduate, I have acquired the skills to build robust applications using a wide range of modern technologies. I love this! I really enjoy the challenge of solving complex problems and the immediate satisfaction of fixing a bug. I also love the collaboration needed to meet the needs of the end user. After working on over thirty projects, my eyes are now primed for the web development world.
           </p>
       </div>
     </>
