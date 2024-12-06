bb0b13291fb9ae7735a4d2aaa6d2113004c0dcde
Theon87 <theonfrancis17@gmail.com>
2024-12-06 11:06:57 -0500

    push to staging

--- src/components/pages/AboutMe.jsx
+++ src/components/pages/AboutMe.jsx
@@ -5,7 +5,7 @@ const AboutMe = () => {
     <>
       <h1>About Me</h1>
       <div className='about-me'>
-          {/* <img src='../src/assets/images/profilephoto.jpg' alt='photo of Theon Francis' className='profilePhoto'/> */}
+          <img src='../src/assets/images/profilephoto.jpg' alt='photo of Theon Francis' className='profilePhoto'/>
           <p>
             I was born and raised in Trinidad and Tobago. I love to travel, so I thought, why not move to the United States to study biology at Lincoln University? After completing my undergraduate degree, I returned to Trinidad to work in education, and then I came back to Maryland to work in education for seven more years. I love watching and playing football (soccer) and have coached the sport for seven years. In my spare time, I also like to play chess for fun.
           </p>
