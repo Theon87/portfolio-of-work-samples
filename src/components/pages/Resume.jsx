71b6fe14451a0d2b0560cacca329f1d57e6490f1
Theon87 <theonfrancis17@gmail.com>
2024-12-09 16:34:33 -0500

    add skills to resume tab

--- src/components/pages/Resume.jsx
+++ src/components/pages/Resume.jsx
@@ -4,10 +4,30 @@ const Resume = () => {
   return (
     <>
       <h1>Resume</h1>
-      <div className='resume'>
-        <p>Request a copy of my resume by emailing me at <a href='mailto:'>theonfrancis17@gmail.com</a></p> <br/>
-        <p>Or you can contact me using the links in the footer below</p>
-      </div>
+      <section className='resume'>
+        <h3>Front-end Proficiencies</h3>
+        <ul>
+          <li>HTML</li>
+          <li>CSS</li>
+          <li>JavaScript</li>
+          <li>React.js</li>
+          <li>Bootstrap</li>
+          <li>Tailwind CSS</li>
+          <li>Semantic UI</li>
+        </ul>
+        <h3>Back-end Proficiencies</h3>
+        <ul>
+          <li>Node.js</li>
+          <li>Express.js</li>
+          <li>TypeScript</li>
+          <li>PostgreSQL</li>
+          <li>MongoDB</li>
+          <li>Mongoose ODM</li>
+          <li>GraphQL</li>
+        </ul>
+        <br />
+        <p>Request a copy of my full resume by using the contact form on the contact tab.</p>
+      </section>
     </>
   )
 }
