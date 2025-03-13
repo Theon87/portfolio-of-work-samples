f6abf7f966f217eef30b7e4eab0a2e938a7ccc7f
Theon87 <theonfrancis17@gmail.com>
2025-03-13 13:48:18 -0400

    updates Resume to include Python

--- src/components/pages/Resume.jsx
+++ src/components/pages/Resume.jsx
@@ -1,35 +1,39 @@
-import React from 'react'
+import React from "react";
 
 const Resume = () => {
-  return (
-    <>
-      <h1>Resume</h1>
-      <section className='resume'>
-        <h3>Front-end Proficiencies</h3>
-        <ul>
-          <li>HTML</li>
-          <li>CSS</li>
-          <li>JavaScript</li>
-          <li>React.js</li>
-          <li>Bootstrap</li>
-          <li>Tailwind CSS</li>
-          <li>Semantic UI</li>
-        </ul>
-        <h3>Back-end Proficiencies</h3>
-        <ul>
-          <li>Node.js</li>
-          <li>Express.js</li>
-          <li>TypeScript</li>
-          <li>PostgreSQL</li>
-          <li>MongoDB</li>
-          <li>Mongoose ODM</li>
-          <li>GraphQL</li>
-        </ul>
-        <br />
-        <p>Request a copy of my full resume by using the contact form on the contact tab.</p>
-      </section>
-    </>
-  )
-}
+ return (
+  <>
+   <h1>Resume</h1>
+   <section className="resume">
+    <h3>Front-end Proficiencies</h3>
+    <ul>
+     <li>HTML</li>
+     <li>CSS</li>
+     <li>JavaScript</li>
+     <li>React.js</li>
+     <li>Bootstrap</li>
+     <li>Tailwind CSS</li>
+     <li>Semantic UI</li>
+    </ul>
+    <h3>Back-end Proficiencies</h3>
+    <ul>
+     <li>Node.js</li>
+     <li>Express.js</li>
+     <li>TypeScript</li>
+     <li>PostgreSQL</li>
+     <li>MongoDB</li>
+     <li>Mongoose ODM</li>
+     <li>GraphQL</li>
+     <li>Python</li>
+    </ul>
+    <br />
+    <p>
+     Request a copy of my full resume by using the contact form on the contact
+     tab.
+    </p>
+   </section>
+  </>
+ );
+};
 
-export default Resume
+export default Resume;
